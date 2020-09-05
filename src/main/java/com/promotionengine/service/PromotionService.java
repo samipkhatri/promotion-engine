2a277f2eed03cda42aceb4f2475b4d5b178eb9b9
Samip Khatri <khatri.samip24@gmail.com>
2020-09-05 17:29:32 +0530

    add PromotionService class and add initial promotions

---
+++ src/main/java/com/promotionengine/service/PromotionService.java
@@ -0,0 +1,27 @@
+package com.promotionengine.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.promotionengine.promotions.Promotion;
+import com.promotionengine.promotions.Promotion1;
+import com.promotionengine.promotions.Promotion2;
+import com.promotionengine.promotions.Promotion3;
+
+public class PromotionService {
+
+	private static final List<Promotion> PROMOTIONS;
+	
+	// initial promotions to apply
+	static {
+		PROMOTIONS = new ArrayList<>();
+		PROMOTIONS.add(new Promotion1());
+		PROMOTIONS.add(new Promotion2());
+		PROMOTIONS.add(new Promotion3());
+	}
+	
+	// To add promotions in future
+	public void addPromotions(Promotion promo) {
+		PROMOTIONS.add(promo);
+	} 
+}
