9a16784709b0c7ad1ce2642085de84458773760c
Samip Khatri <khatri.samip24@gmail.com>
2020-09-05 16:24:22 +0530

    add mockito and test methods signature

--- src/test/java/com/promotionengine/PromotionEngineTest.java
+++ src/test/java/com/promotionengine/PromotionEngineTest.java
@@ -1,5 +1,35 @@
 package com.promotionengine;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
 public class PromotionEngineTest {
+	
+	@InjectMocks
+	PromotionEngine promotionEngine;
+	
+	@Before
+	public void intit() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void scenarioA() {
+		
+	}
+	
+	@Test
+	public void scenarioB() {
+		
+	}
+	
+	@Test
+	public void scenarioC() {
+		
+	}
+	
+	
 
 }
